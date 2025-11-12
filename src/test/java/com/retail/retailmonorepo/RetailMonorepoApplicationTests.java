02371334b0f5c4f31b2e4e2b19d95379153599fe
SoonWookHwang <bravadosw@naver.com>
2025-11-12 14:09:56 +0900

    chore: initialize retail monorepo structure with multi-module setup

---
+++ src/test/java/com/retail/retailmonorepo/RetailMonorepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.retailmonorepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailMonorepoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
